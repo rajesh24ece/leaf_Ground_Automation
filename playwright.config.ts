e8eff5699ffc0c220a649aa29d0c8cdbd885eca4
Rajesh Kumar Pandian <prajeshkumar@outlook.com>
2026-06-21 07:47:58 +0530

    uploadAndChanges

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [["html"], ["allure-playwright"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -27,7 +28,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
-    headless: false,
+    headless: !!process.env.CI,
     screenshot: "on",
   },
 
