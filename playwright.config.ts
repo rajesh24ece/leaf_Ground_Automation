1ec32a68189bbd4aac7080f25e4a07ed5f5836fd
Rajesh Kumar Pandian <prajeshkumar@outlook.com>
2026-06-23 21:52:36 +0530

    report changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,11 @@ export default defineConfig({
   //workers: process.env.CI ? 1 : undefined,
   workers: Number(process.env.WORKERS) || 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["html"], ["allure-playwright"]],
+  reporter: [
+    ["html"],
+    ["allure-playwright"],
+    ["json", { outputFile: "test-results.json" }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
