a1d9081874665da51c955b57e2312dc938786001
Rajesh Kumar Pandian <prajeshkumar@outlook.com>
2026-06-24 18:55:59 +0530

    Added Playwright artifact cleanup and gitignore updates

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,83 +1,72 @@
 import { defineConfig, devices } from "@playwright/test";
-
 import dotenv from "dotenv";
 import path from "path";
-dotenv.config({ path: path.resolve(__dirname, ".env.leafground") });
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
+dotenv.config({
+  path: path.resolve(__dirname, ".env.leafground"),
+});
+
 export default defineConfig({
   testDir: "./tests",
-  /* Run tests in files in parallel */
+
+  // Cleanup before execution
+  globalSetup: require.resolve("./globalSetup"),
+
+  // Store screenshots, traces, videos etc.
+  outputDir: "test-results",
+
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
+
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
+
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  //workers: process.env.CI ? 1 : undefined,
+
   workers: Number(process.env.WORKERS) || 1,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+
   reporter: [
     ["html"],
     ["allure-playwright"],
     ["json", { outputFile: "test-results.json" }],
   ],
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
     baseURL: process.env.BASE_URL,
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
+
     headless: !!process.env.CI,
+
     screenshot: "on",
+
+    video: "retain-on-failure",
   },
 
-  /* Configure projects for major browsers */
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      use: {
+        ...devices["Desktop Chrome"],
+      },
     },
 
     // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
+    //   name: "firefox",
+    //   use: { ...devices["Desktop Firefox"] },
     // },
 
     // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
+    //   name: "webkit",
+    //   use: { ...devices["Desktop Safari"] },
     // },
 
-    /* Test against mobile viewports. */
     // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
+    //   name: "Mobile Chrome",
+    //   use: { ...devices["Pixel 5"] },
     // },
 
-    /* Test against branded browsers. */
     // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    //   name: "Mobile Safari",
+    //   use: { ...devices["iPhone 12"] },
     // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
