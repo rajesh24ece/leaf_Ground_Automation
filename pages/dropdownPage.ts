719e5e0bea98dc8f454fb01261ad5a23ca760b8d
Rajesh Kumar Pandian <prajeshkumar@outlook.com>
2026-06-24 17:40:16 +0530

    fileRename

--- pages/dropdownPage.ts
+++ pages/dropdownPage.ts
@@ -1,4 +1,4 @@
-import { expect, Page, test } from "@playwright/test";
+import { expect, Page } from "@playwright/test";
 import { Methods } from "../utils/methods";
 import { DropdownTestData } from "../utils/test-data.interface";
 import { DropDownLocators } from "../locators/dropDownLocators";
