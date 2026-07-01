0d2d85ab1873293595ced3127324d43bed070624
Rajesh Kumar Pandian <prajeshkumar@outlook.com>
2026-07-01 08:01:17 +0530

    Revamp V5

--- tests/Dropdown.spec.ts
+++ tests/Dropdown.spec.ts
@@ -20,24 +20,24 @@ test.afterAll(async () => {
 });
 
 test("Select favourite UI automation tool.", async () => {
-  await test.step("Which is your favorite UI Automation tool?", async () => {
-    await dropdownPage.selectTool(data);
-  });
-
   await test.step("Country is loaded and selected correctly.", async () => {
     await dropdownPage.selectCountry(data);
+  });
+
+  await test.step("Which is your favorite UI Automation tool?", async () => {
+    await dropdownPage.selectTool(data);
   });
 
   await test.step("Selecting Confirm Cities belongs to Country is loaded.", async () => {
     await dropdownPage.selectCity(data);
   });
 
-  await test.step("Selecting the Course.", async () => {
-    await dropdownPage.selectCourse(data);
-  });
-
   await test.step("Select language to select the two from the following dropdown.", async () => {
     await dropdownPage.selectLanguage(data);
+  });
+
+  await test.step("Selecting the Course.", async () => {
+    await dropdownPage.selectCourse(data);
   });
 
   await test.step("Select 'Two' irrespective of the language chosen.", async () => {
